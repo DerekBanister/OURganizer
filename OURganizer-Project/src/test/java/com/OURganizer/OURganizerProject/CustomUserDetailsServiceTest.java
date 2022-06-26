5d65a64f054c8c65a433364a7220e0fc4c5f24d0
unknown <ptownderek@yahoo.com>
2022-06-26 12:26:27 -0700

    custom user details service test is passing

--- OURganizer-Project/src/test/java/com/OURganizer/OURganizerProject/CustomUserDetailsServiceTest.java
+++ OURganizer-Project/src/test/java/com/OURganizer/OURganizerProject/CustomUserDetailsServiceTest.java
@@ -3,14 +3,42 @@ package com.OURganizer.OURganizerProject;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.OURganizer.OURganizerProject.model.User;
+import com.OURganizer.OURganizerProject.repository.UserRepository;
 
 @SpringBootTest
 class CustomUserDetailsServiceTest {
-
+	
+	
+	@Autowired
+	private CustomUserDetailsService customService;
+	
+	@Autowired UserRepository userRepository;
+	
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		User user = new User();
+		
+		user.setId((long) 15);
+		user.setEmail("duck@gmail.com");
+		user.setFirstName("ddbo");
+		user.setLastName("banny");
+		user.setPassword("him");
+		
+		userRepository.save(user);
+		
+		
+		String actual = customService.loadUserByUsername("duck@gmail.com").getUsername();
+		
+		
+		String expected = user.getEmail();
+		
+		assertEquals(actual, expected);
+		
 	}
 
 }
